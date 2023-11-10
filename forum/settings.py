fc2ee66ac196eec4904a13d33e2942e4858d3606
antoniow5 <118053208+antoniow5@users.noreply.github.com>
2023-11-10 22:57:56 +0900

    Update settings.py

--- forum/settings.py
+++ forum/settings.py
@@ -83,6 +83,15 @@ DATABASES = {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
+    #     'default': {
+    #     'ENGINE': 'django.db.backends.postgresql',
+    #     'NAME': 'forum',
+    #     'USER': 'admin',
+    #     'PASSWORD': 'pass',
+    #     'HOST': 'localhost',
+    #     'PORT': '',
+    # }
+
 }
 
 
