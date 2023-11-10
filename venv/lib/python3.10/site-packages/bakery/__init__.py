afb0f52195a837adbaf173035c8472dd6e589ff4
antoniow5 <118053208+antoniow5@users.noreply.github.com>
2023-11-10 22:55:00 +0900

    Delete venv directory

--- venv/lib/python3.10/site-packages/bakery/__init__.py
+++
@@ -1,7 +0,0 @@
-try:
-    from dataclasses import dataclass
-except ImportError:
-    pass
-from bakery.assertions import (assert_equal, assert_type,
-                                assert_true, assert_false,
-                                QUIET, student_tests)
